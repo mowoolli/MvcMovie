2ebe308f7e5c5a8a626abb99de7b0fb997adde3f
Marc Woolliscroft <mwoolli@gmail.com>
2020-05-20 21:45:33 -0700

    Start adding model classes for movies
    Need to get entity framework core using Nuget.

---
+++ Models/MvcMovieContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovie.Models
+{
+  public class MvcMovieContext : DbContext
+  {
+    public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+    : base(options)
+    { }
+    public DbSet<Movie> Movie { get; set; }
+  }
+}
